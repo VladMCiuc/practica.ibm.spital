6b39d594171066d947831b19279cc2e5c8597fc9
Bogdan Maceasa <bogdan.maceasa@ibm.com>
2023-07-21 20:28:44 +0300

    Building Controller endpoints
    - adding logic to REST Endpoints in Controller class
    - defining DTOs used in the Controller class
    - created Service class and methods

--- src/main/java/com/ibm/practica/spital/controller/SpitalController.java
+++ src/main/java/com/ibm/practica/spital/controller/SpitalController.java
@@ -1,36 +1,64 @@
 package com.ibm.practica.spital.controller;
 
+import com.ibm.practica.spital.DTO.AddReservation;
+import com.ibm.practica.spital.DTO.Pacient;
+import com.ibm.practica.spital.DTO.Reservation;
+import com.ibm.practica.spital.service.SpitalService;
 import lombok.extern.log4j.Log4j2;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Log4j2
 @RestController
 @RequestMapping("/spital")
 public class SpitalController {
 
-    @GetMapping("/all")
-    public String getAllPacients(){
-        log.info("getAllPacients has started...");
-        return "Test";
+    @Autowired
+    private SpitalService service;
+
+    @GetMapping("/getAllPacients")
+    public List<Pacient> getAllPacients(){
+        log.info("SpitalController.getAllPacients() has started...");
+        List<Pacient> result = service.getAllPacients();
+
+        log.info("SpitalController.getAllPacients() has finished.");
+        return result;
     }
 
     @GetMapping("/reservations")
-    public String getReservations(){
-        return "Reservations";
+    public List<Reservation> getReservations(){
+        return service.getReservations();
     }
-
     @GetMapping("/reservation")
-    public String getReservationForPacient(int id){
-        return "Reservation for Customer";
+    public Reservation getReservation(String reservationID){
+        return service.getReservation();
+    }
+    @GetMapping("/getPacientReservation")
+    public ResponseEntity<List<Reservation>> getReservationForPacient(String pacientID){
+
+        if(service.getReservationForPacient(pacientID).isEmpty()){
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+
+        return ResponseEntity.of(Optional.ofNullable(service.getReservationForPacient(pacientID)));
     }
 
     @PostMapping("/addReservation")
-    public String addReservation(){
-        return "Reservation added";
+    public ResponseEntity addReservation(AddReservation reservation){
+
+        return service.addReservation(reservation) ? ResponseEntity.ok().build() : ResponseEntity.badRequest().build();
     }
 
     @PostMapping("/addPacient")
@@ -39,8 +67,13 @@ public class SpitalController {
     }
 
     @DeleteMapping("/deleteReservation")
-    public String deleteReservation(){
-        return "Reservation deleted";
+    public ResponseEntity deleteReservation(String reservationID){
+
+        if(service.deleteReservation(reservationID)){
+            ResponseEntity.ok().build();
+        } else ResponseEntity.notFound().build();
+
+        return service.deleteReservation(reservationID) ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/deletePacient")
